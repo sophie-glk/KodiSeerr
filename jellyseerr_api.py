93b4b5c8c346e52139fa360dbc2c871d8a5d56aa
soph.glk <sophieglueck@mailbox.org>
2026-03-05 17:38:48 +0100

    fix api function signatures

--- jellyseerr_api.py
+++ jellyseerr_api.py
@@ -2,5 +2,5 @@ from ApiClient import ApiClient
 
 class JellyseerrClient(ApiClient):
     name = "jellyseerr"
-    def __init__(self, server_url,  api_token, server_url_4k=None, api_token_4k=None):
+    def __init__(self, server_url,  api_token, has_4k=False, server_url_4k=None, api_token_4k=None):
        super().__init__(f"{server_url}/api/v1", api_token)
--- sonarr_api.py
+++ sonarr_api.py
@@ -2,7 +2,7 @@ from ApiClient import ApiClient
 
 class SonarrClient(ApiClient):
     name = "sonarr"
-    def __init__(self, server_url,  api_token, server_url_4k=None, api_token_4k=None):
+    def __init__(self, server_url,  api_token, has_4k=False, server_url_4k=None, api_token_4k=None):
        if server_url_4k is not None:
            server_url_4k = f"{server_url_4k}/api/v3"
-       super().__init__(f"{server_url}/api/v3", api_token, server_url_4k, api_token_4k)
+       super().__init__(f"{server_url}/api/v3", api_token, has_4k, server_url_4k, api_token_4k)
