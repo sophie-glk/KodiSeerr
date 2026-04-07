91e4e645f7bf9e3ba782651a931cba77eef6b7ad
soph.glk <sophieglueck@mailbox.org>
2026-04-07 16:51:06 +0200

    fix sonarr_client function signature

--- apis/sonarr_api.py
+++ apis/sonarr_api.py
@@ -2,10 +2,10 @@ from apis.ApiClient import ApiClient
 
 class SonarrClient(ApiClient):
     name = "sonarr"
-    def __init__(self, server_url,  api_token, has_4k=False, server_url_4k=None, api_token_4k=None):
+    def __init__(self, server_url,  api_token, has_4k=False, server_url_4k=None, api_token_4k=None, allow_self_signed = False):
        if server_url_4k is not None:
            server_url_4k = f"{server_url_4k}/api/v3"
-       super().__init__(f"{server_url}/api/v3", api_token, has_4k, server_url_4k, api_token_4k)
+       super().__init__(f"{server_url}/api/v3", api_token, has_4k, server_url_4k, api_token_4k, allow_self_signed)
 
     def _handle_status_code(self, status_code):
         if status_code in [200, 201, 202, 204]:
